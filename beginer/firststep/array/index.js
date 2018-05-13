5d0ae9062b980642b75b3fa9766947d8dfd854e8
phamvanoanh <oanhmrphamvan@gmail.com>
2018-05-13 11:52:01 +0700

    first commit

---
+++ beginer/firststep/array/index.js
@@ -0,0 +1,9 @@
+console.log("pham vanoanh");
+
+var name = ['oanh', 'phuong', 'luan', 'quynh'];
+console.log(name);
+
+var tt = [name, 'ha nam', 12323];
+console.log(tt);
+
+// accessing and modify array items
