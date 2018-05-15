8550035010f68a1e32da921a1cc7fc83683f51dd
phamvanoanh <oanhmrphamvan@gmail.com>
2018-05-15 21:40:15 +0700

    assignment does not finsish

---
+++ beginer/firststep/assignment/main.js
@@ -0,0 +1,29 @@
+"1.COMPLETE VARIABLE AND FUNCTION DEFINITIONS"
+var customName = '';
+// còn hai biến nữa
+var randomize = document.querySelector()
+console.log(randomize);
+
+var story = document.querySelector('p');
+
+function randomValueFromArray() {
+    
+}
+
+"2. RAW TEXT STRINGS"
+
+var storyText = "";
+var insertX = [];
+var insertY = [];
+var insertZ = [];
+"3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION"
+
+// randomize.click(result());
+
+function result() {
+    var newStory = storyText;
+
+    xItem = randomValueFromArray();
+    yItem = randomValueFromArray();
+    zItem = randomValueFromArray();
+}
