aed6b2680c5ce4069c866b2455d112e617fb26cd
phamvanoanh <oanhmrphamvan@gmail.com>
2018-07-15 16:26:42 +0700

    add text formatting

---
+++ Javascript Guide/Textformatting.js
@@ -0,0 +1,25 @@
+console.log('\u{2F804}');
+
+/**
+ * String object
+ */
+
+ var str = new String ('oanhpv');
+ console.log(str);
+ console.log(typeof str);
+ 
+ var s2 = new String('2 + 2');
+ var s1 = '2 + 2';
+
+ console.log(`S1  : ${s1}`);
+ console.log(s1);
+ console.log(s2);
+ console.log(`S2  : ${s2}`);
+ // Multiple lines
+ console.log(`
+  pham van oanh
+  hhaha
+  hfdffh
+  fdfdf
+ `);
+ 
